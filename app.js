31effd5def3498eaa5c7c0073723f35220203060
srivastavarahul442 <srivastavarahul442@gmail.com>
2024-08-23 23:35:07 +0530

    First commit

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let newGameBtn = document.querySelector("#new-btn");
 let msgContainer = document.querySelector(".msg-container");
 let msg = document.querySelector("#msg");
 
+
 let turn0 = true;
 
 const winPattern = [
